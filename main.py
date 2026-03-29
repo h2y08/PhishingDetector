cb36d0b377d02767a85f09717d08ff03edf9824d
h2y08 <1927690704@qq.com>
2026-03-29 21:53:28 +0800

    修改了访问浏览器的防机器人验证伪装

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ class MultiModalDetector:
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--ignore-certificate-errors')
             options.add_argument(
-                'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/146.0.0.0 Safari/537.36')
+                'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36')
 
             driver = webdriver.Chrome(options=options)
             driver.set_page_load_timeout(15)
@@ -261,7 +261,7 @@ class PhishingDetectorApp(QMainWindow):
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--ignore-certificate-errors')
             options.add_argument(
-                'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/146.0.0.0 Safari/537.36')
+                'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36')
 
             driver = webdriver.Chrome(options=options)
             driver.set_page_load_timeout(15)
